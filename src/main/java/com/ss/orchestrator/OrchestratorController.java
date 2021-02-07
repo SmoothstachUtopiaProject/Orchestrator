392bd11dcbe4bee501fedc6a539a1093f5907a22
Joshua Tyler <Joshua.Tyler@smoothstack.com>
2021-02-06 21:01:07 -0500

    Added Actuator

--- src/main/java/com/ss/orchestrator/OrchestratorController.java
+++ src/main/java/com/ss/orchestrator/OrchestratorController.java
@@ -6,11 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 
+@CrossOrigin(origins = "http://localhost:8080")
 @RestController
 public class OrchestratorController {
 
